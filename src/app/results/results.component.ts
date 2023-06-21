2bd8ff1bfa65346bb67234808427dacec8a8b82f
jojupiter <sedoufofe@hmail.com>
2023-06-21 04:12:50 -0400

    add no dim

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -13,6 +13,11 @@ export class ResultsComponent implements OnInit {
   modele1:any;
   modele2:any;
   modele3:any;
+  modele4:any;
+  modele5:any;
+  modele6:any;
+  modele7:any;
+
 
   constructor(private appService: AppServiceService) { }
 
@@ -22,7 +27,11 @@ export class ResultsComponent implements OnInit {
     this.modele1=this.results[1]
     this.modele2=this.results[2]
     this.modele3=this.results[3]
-    
+    this.modele4=this.results[4]
+    this.modele5=this.results[5]
+    this.modele6=this.results[6]
+    this.modele7=this.results[7]
+
   }
 
   getResultSpam(spam:boolean):String {
